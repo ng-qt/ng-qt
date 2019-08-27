9825187becba371633bbadfaa6b07adf21d90f8d
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-08-27 15:02:39 +0200

    refactor(builders): remove unnecessary fn

--- packages/builders/src/build/utils.ts
+++ packages/builders/src/build/utils.ts
@@ -1,29 +1,7 @@
-import { Path, normalize } from '@angular-devkit/core';
+import { normalize } from '@angular-devkit/core';
 import { resolve, dirname, relative, basename } from 'path';
 import { BuildOptions } from './types';
 import { statSync } from 'fs';
-
-export const replaceNewLineChars = (someString: string, replacementString = '') => {
-  // defaults to just removing
-  const LF = `\u{000a}`; // Line Feed (\n)
-  const VT = `\u{000b}`; // Vertical Tab
-  const FF = `\u{000c}`; // Form Feed
-  const CR = `\u{000d}`; // Carriage Return (\r)
-  const CRLF = `${CR}${LF}`; // (\r\n)
-  const NEL = `\u{0085}`; // Next Line
-  const LS = `\u{2028}`; // Line Separator
-  const PS = `\u{2029}`; // Paragraph Separator
-  const lineTerminators = [LF, VT, FF, CR, CRLF, NEL, LS, PS]; // all Unicode `lineTerminators`
-  let finalString = someString.normalize(`NFD`); // better safe than sorry? Or is it?
-  for (let lineTerminator of lineTerminators) {
-    if (finalString.includes(lineTerminator)) {
-      // check if the string contains the current `lineTerminator`
-      let regex = new RegExp(lineTerminator.normalize(`NFD`), `gu`); // create the `regex` for the current `lineTerminator`
-      finalString = finalString.replace(regex, replacementString); // perform the replacement
-    }
-  }
-  return finalString.normalize(`NFC`); // return the `finalString` (without any Unicode `lineTerminators`)
-};
 
 export interface FileReplacement {
   replace: string;
