67fe5d0f987c1bcfd7ee1338f3ec0c66f98beb9d
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-08-27 15:48:53 +0200

    chore(): remove irrelevant files

--- packages/core/src/error-handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const core_1 = require('@angular/core');
-function errorHandlerFactory() {
-  return new core_1.ErrorHandler();
-}
-exports.errorHandlerFactory = errorHandlerFactory;
-//# sourceMappingURL=error-handler.js.map
