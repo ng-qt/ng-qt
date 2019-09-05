698e28fc8f26499017d64ed1143c40ec20f08559
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-09-05 13:54:49 +0200

    test(core): view util insert child

--- packages/core/src/renderer/test/view-util.spec.ts
+++ packages/core/src/renderer/test/view-util.spec.ts
@@ -1,11 +1,11 @@
-import { NgQtView } from '@ng-qt/common';
+import { CommentNode, NgQtView } from '@ng-qt/common';
 import { View } from '@ng-qt/common/widgets/view';
 
 import { ViewUtil } from '../view-util';
 
 describe('ViewUtil', () => {
-  let viewUtil: ViewUtil;
-  let parent: NgQtView;
+  let viewUtil: any | ViewUtil;
+  let parent: any | NgQtView;
 
   beforeEach(() => {
     viewUtil = new ViewUtil();
@@ -14,18 +14,31 @@ describe('ViewUtil', () => {
 
   describe('insertChild', () => {
     beforeEach(() => {
-      // @ts-ignore
       viewUtil.addToQueue = jest.fn();
-      // @ts-ignore
       viewUtil.findNextVisual = jest.fn();
-      // @ts-ignore
       viewUtil.addToVisualTree = jest.fn();
     });
 
-    it(`should add parent to child's parentNode`, () => {
-      const child: Ngq = new View();
+    it(`should not add child to visual tree if it's a detached element`, () => {
+      const child: any = new CommentNode(null);
 
-      // @ts-ignore
+      viewUtil.insertChild(parent, child);
+
+      expect(viewUtil.addToVisualTree).not.toHaveBeenCalled();
+    });
+
+    it(`should add parent's lastChild as previous if it's nil`, () => {
+      const child: any = new View();
+      parent.lastChild = child;
+
+      viewUtil.insertChild(parent, child);
+
+      expect(viewUtil.addToQueue).toHaveBeenCalledWith(parent, child, parent.lastChild, undefined);
+    });
+
+    it(`should add parent to child's parentNode`, () => {
+      const child: any = new View();
+
       viewUtil.insertChild(parent, child);
 
       expect(child.parentNode).toBe(parent);
