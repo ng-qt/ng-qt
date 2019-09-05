a7c8ad4428814115e12d6a63a3276fe3657b058d
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-09-05 16:11:01 +0200

    WIP

--- packages/testing/src/platform-dynamic-testing.ts
+++ packages/testing/src/platform-dynamic-testing.ts
@@ -1,10 +1,10 @@
 import { createPlatformFactory } from '@angular/core';
 import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { NgQtPlatformRef } from '@ng-qt/common';
 import {
   NGQT_COMPILER_PROVIDERS,
   NGQT_INTERNAL_PLATFORM_PROVIDERS,
 } from '@ng-qt/platform/src/providers';
-import { NgQtPlatformRef } from '@ng-qt/common';
 
 // TODO
 export const platformDynamicTesting: NgQtPlatformRef = createPlatformFactory(
